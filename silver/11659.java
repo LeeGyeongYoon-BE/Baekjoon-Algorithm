6958f7b2abb59d13a2e545c4de601afa15e26d97
LeeGyeongYoon-BE <jack3614@naver.com>
2025-11-11 09:46:07 +0900

    Create 11659.java

---
+++ silver/11659.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        } // for
+        int[] acc = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            acc[i] = acc[i-1] + arr[i];
+        } // for
+        while (m-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int l = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
+            System.out.println(acc[r]-acc[l-1]);
+        } // while
+
+        br.close();
+    } // main
+} // class
