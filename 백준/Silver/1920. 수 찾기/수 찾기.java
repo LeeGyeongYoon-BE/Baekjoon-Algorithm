8ff85a7a5638dfa8c0def37680ade9da26fcacea
LeeGyeongYoon-BE <jack3614@naver.com>
2025-11-13 14:52:45 +0900

    [Silver IV] Title: 수 찾기, Time: 1108 ms, Memory: 58392 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,38 +1,21 @@
 import java.io.*;
 import java.util.*;
+
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N =  Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        Set<Integer> set = new HashSet<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] A = new int[N];
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
         }
-        Arrays.sort(A);
-        int M =  Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            int target =  Integer.parseInt(st.nextToken());
-            int start = 0;
-            int end = A.length - 1;
-            boolean find = false;
-            while (start <= end) {
-                int mid = (start + end)/2;
-                if (A[mid] > target) {
-                    end = mid-1;
-                } else if (A[mid] < target) {
-                    start = mid+1;
-                } else {
-                    find = true;
-                    break;
-                }
-            }
-            if (find) {
-                System.out.println(1);
-            } else {
-                System.out.println(0);
-            }
+        for (int i = 0; i < m; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            System.out.println(set.contains(x) ? 1 : 0);
         }
     }
 }
