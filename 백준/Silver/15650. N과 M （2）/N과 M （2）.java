fce943428caa68f443c89928c55b360c793839a1
LeeGyeongYoon-BE <jack3614@naver.com>
2025-12-05 13:35:03 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int n, m;
+    static StringBuilder sb = new StringBuilder();
+    static int[] selected;
+
+    static void rec_func(int k){
+        if (k == m + 1){
+            for (int i = 1; i <= m; i++){
+                sb.append(selected[i]).append(" ");
+            }
+            sb.append("\n");
+        } else {
+            for (int cand = selected[k-1]+1; cand <= n; cand++){
+                selected[k] = cand;
+                rec_func(k+1);
+                selected[k] = 0;
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        selected = new int[m+1];
+
+        rec_func(1);
+        System.out.println(sb);
+    }
+}
