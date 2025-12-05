5fe32411ce0b7d10755361edaa63fc2668d30e1f
LeeGyeongYoon-BE <jack3614@naver.com>
2025-12-05 11:41:42 +0900

    [Silver III] Title: N과 M (3), Time: 388 ms, Memory: 67836 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,32 +1,38 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-    static int n, m;
+public class Main {
+    static int N, M;
+    static int[] list;
     static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        list = new int[N];
 
-        bruteForce(new int[m], 0);
+        permutation(0);
+
         System.out.println(sb);
-    }
+        br.close();
+    } // main
 
-    public static void bruteForce(int[] arr, int depth){
-        if (depth == m){
-            for (int num : arr){
-                sb.append(num).append(" ");
+    private static void permutation(int idx){
+
+        if (idx >= M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(list[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
+        for (int i = 1; i <= N; i++) {
 
-        for (int i = 1; i <= n; i++){
-            arr[depth] = i;
-            bruteForce(arr, depth + 1);
-        }
-    }
+            list[idx] = i;
+            permutation( idx + 1 );
 
-}
+        } // for
+
+    } // permutation
+} // class
